5ebdd031f1409949d7469c5842c38df26e1cdd2b
ax <bacz.al@gmail.com>
2019-10-02 13:05:46 -0500

    FIX single.php
    el post de BLOG

--- single.php
+++ single.php
@@ -11,39 +11,48 @@
         <article id="post-<?php the_ID(); ?>" <?php post_class('blog-article'); ?>>
         <!-- <h1> single.php </h1> -->
 
-            <div class="d-flex my-4" style="background-color:#eee;">
+            <!--<div class="d-flex my-4" style="background-color:#eee;height: 450px;margin: 0;  position: relative;  padding: 0 0 40px;">-->
+            <div class="b-image" style="">
             <!-- post thumbnail -->
             <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
                 <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
                     <?php the_post_thumbnail('blog-size', array('class' => 'img-fluid')); ?>
                 </a>
             <?php endif; ?>
-            <!-- /post thumbnail -->                
-            </div>
-
-            <!-- post title -->
-            <h1 class="blog-title">
+            <!-- /post thumbnail -->          
+                         <!-- post title -->
+                         <h1 class="blog-title">
                 <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
             </h1>
-            <!-- /post title -->
+            <!-- /post title -->      
+            </div>
 
-            <!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'ave' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'ave' ), __( '1 Comment', 'ave' ), __( '% Comments', 'ave' )); ?></span>
-			<!-- /post details -->
 
+            
+            <div class="b-content my-4" style="outline:2px solid blue;">
+            
+                <div class="blog-meta">
+                <!-- post details -->
+                <span class="blog-date date"><?php the_time('F j, Y'); ?> <!--<?php the_time('g:i a'); ?>--></span>
+                <span class="blog-date"><?php _e( ' ', 'ave' ); the_category(', '); // Separated by commas ?></span>
+
+                <!--<span class="author"><?php _e( 'Published by', 'ave' ); ?> <?php the_author_posts_link(); ?></span>
+                <span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'ave' ), __( '1 Comment', 'ave' ), __( '% Comments', 'ave' )); ?></span>-->
+                <!-- /post details -->
+                </div>
+
+            
             <?php the_content(); // Dynamic Content ?>
 
-            <?php the_tags( __( 'Tags: ', 'ave' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+            <!-- SE VA -->
+                <?php the_tags( __( 'Tags: ', 'ave' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+                <p><?php _e( 'This post was written by ', 'ave' ); the_author(); ?></p>
+                <?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+                <!-- <?php comments_template(); ?> -->
+            
 
-            <p><?php _e( 'Categorised in: ', 'ave' ); the_category(', '); // Separated by commas ?></p>
-
-            <p><?php _e( 'This post was written by ', 'ave' ); the_author(); ?></p>
-
-            <?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-
-            <?php comments_template(); ?>
+            </div>
+           
 
         </article>
         <!-- /  article -->
@@ -58,7 +67,36 @@
 		</article>
 		<!-- /article -->
 
-	<?php endif; ?>
+    <?php endif; ?>
+    
+    <div class="b-trips my-4" style="outline:2px solid green;">
+        
+        <!-- inicio tours -->
+
+            <section id="tours">
+
+            <div class="container">
+                <div class="row mt-4">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="section-heading">Tours destacados </h2>
+                        <hr class="my-4">
+                    </div>
+                </div>
+            </div>
+
+            <div id="tours-block-1" class="container">
+                <div class="row">
+                        <?php ave_query_tours(3); ?>
+                </div>
+            </div>
+
+            </section> 
+
+        <!-- fin tours -->
+
+    </div>
+    
+    
     
     </div>
 
